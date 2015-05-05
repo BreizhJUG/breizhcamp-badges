ba338b75910d29a222fe1449d7771cfc925969f4
Jean-Louis Jouannic <jeanlouis_jouannic@yahoo.fr>
2015-05-05 19:45:18 +0200

    Upgrade gradle (2.4), itext (5.5.5) Ajout plugin cobertura Ajout cas limites sur l'encoding dans les TU de CSVBadgeParser

--- src/test/groovy/org/breizhcamp/badge/CSVBadgeParserSpec.groovy
+++ src/test/groovy/org/breizhcamp/badge/CSVBadgeParserSpec.groovy
@@ -25,18 +25,13 @@ class CSVBadgeParserSpec extends Specification {
     def 'test constructing CSVBadgeParser'() {
 
         given:
-        def input = new ByteArrayInputStream("""lastname,firstname,email,company,ticketType
-,Œdipe,œdipe@mythologie.gr,Complex & Associates,Team
-""".getBytes('UTF-8'))
+        def input = getClass().getResourceAsStream('nominal.csv')
 
         when:
-        parser = new CSVBadgeParser(input, options)
+        parser = new CSVBadgeParser(input, [:])
 
         then:
         parser.linesIterator.hasNext()
-
-        where:
-        options << [[:], [encoding: 'ISO-8859-1']]
     }
 
     def 'test next() with wrong headers in CSV'() {
@@ -77,5 +72,37 @@ class CSVBadgeParserSpec extends Specification {
         csvFile              || expected
         'nominal.csv'        || new Badge(lastname: 'Snow', firstname: 'Jon', email: 'jon.snow@castle-black.wes', company: 'The Night\'s Watch', ticketType: 'Speaker')
         'missing-header.csv' || new Badge(lastname: 'Snow', firstname: 'Jon', email: 'jon.snow@castle-black.wes', company: 'The Night\'s Watch', ticketType: null)
+    }
+
+    def 'test encoding traps'() {
+        given:
+        def input = getClass().getResourceAsStream(csvFile)
+        parser = new CSVBadgeParser(input, [encoding: wrongEncoding])
+
+        expect:
+        parser.hasNext()
+
+        when:
+        Badge content = parser.next()
+
+        then: 'characters are not interpreted correctly'
+        !content.firstname.startsWith(expected)
+
+        where:
+        csvFile                    | wrongEncoding || expected
+        'utf-8-encoded.csv'        | 'ISO-8859-1'  || 'Œ'
+        'windows-1252-encoded.csv' | 'UTF-8'       || 'Œ'
+    }
+
+    def 'test remove() is not supported'() {
+        given:
+        def input = getClass().getResourceAsStream('nominal.csv')
+        parser = new CSVBadgeParser(input, [:])
+
+        when:
+        parser.remove()
+
+        then:
+        thrown(UnsupportedOperationException)
     }
 }
